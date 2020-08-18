9fd8ef907ddd38d5fce03de90f6d64bd67886c12
nlikhopoi <junetomato@yandex.ru>
2020-08-18 10:12:01 +0800

    from 1 to several renders

--- src/components/Page.js
+++ src/components/Page.js
@@ -6,6 +6,19 @@ export class Page extends React.Component {
     const year = +e.currentTarget.innerText
     this.props.getPhotos( year )
   }
+
+  renderButtons = () => {
+    const years = [ 2018, 2017, 2016, 2015, 2014 ]
+
+    return years.map( ( item, index ) => {
+      return (
+        <button key={ index } className="btn" onClick={ this.onBtnClick }>
+          { item }
+        </button>
+      )
+    })
+  }
+
   renderTemplate = () => {
     const { photos, isFetching, error } = this.props
 
@@ -33,23 +46,11 @@ export class Page extends React.Component {
     return (
       <div className="ib page">
         <p>
-          <button className="btn" onClick={ this.onBtnClick }>
-            2018
-          </button>{' '}
-          <button className="btn" onClick={ this.onBtnClick }>
-            2017
-          </button>{' '}
-          <button className="btn" onClick={ this.onBtnClick }>
-            2016
-          </button>{' '}
-          <button className="btn" onClick={ this.onBtnClick }>
-            2015
-          </button>{' '}
-          <button className="btn" onClick={ this.onBtnClick }>
-            2014
-          </button>
+          { this.renderButtons() }
         </p>
-        <h3>{ year } год [{ photos.length }]</h3>
+        <h3>
+          { year } год [{ photos.length }]
+        </h3>
         { this.renderTemplate() }
       </div>
     )
